feef2cd85d505dadc2681d83293cfd88e92cef5c
dfctdns <denisbezrukov2006@gmail.com>
2024-12-16 22:56:35 +0300

    KSP-1-2 created

---
+++ src/main/kotlin/lesson1/lesson2_task2.kt
@@ -0,0 +1,17 @@
+package org.example.lesson1
+
+
+fun main() {
+
+    var gratitudeText: String =  "Спасибо за покупку!"
+    var numberOfOrders: Int = 75
+
+    println(gratitudeText)
+    println(numberOfOrders)
+
+    var numberOfEmployee: Int = 2000
+//    println(numberOfEmployee)
+
+    numberOfEmployee = 1999
+    println(numberOfEmployee)
+}
