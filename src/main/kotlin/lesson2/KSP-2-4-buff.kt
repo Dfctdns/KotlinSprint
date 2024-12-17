4e1a643a0e3434d2cc2d3c90d2c1fb21784a1232
dfctdns <denisbezrukov2006@gmail.com>
2024-12-17 15:47:14 +0300

    KSP-2-4-buff created and done

---
+++ src/main/kotlin/lesson2/KSP-2-4-buff.kt
@@ -0,0 +1,13 @@
+package org.example.lesson2
+
+fun main() {
+
+    val crystalOre = 7
+    val ironOre = 11
+
+    val buffedCrystalOre = (crystalOre * 0.2).toInt()
+    val buffedIronOre = (ironOre * 0.2).toInt()
+
+    println("$buffedCrystalOre, $buffedIronOre")
+
+}
