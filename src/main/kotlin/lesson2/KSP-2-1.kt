ba7b4a7d44a94596f5a7e25b9cceac2e7c5729ae
dfctdns <denisbezrukov2006@gmail.com>
2024-12-18 18:13:36 +0300

    KS-2-1 fixed val

--- src/main/kotlin/lesson2/KSP-2-1.kt
+++ src/main/kotlin/lesson2/KSP-2-1.kt
@@ -3,9 +3,9 @@ package org.example.lesson2
 fun main() {
 
     val firstStudentGrade = 3f
-    val secondStudentGrade = 3f
+    val secondStudentGrade = 4f
     val thirdStudentGrade = 3f
-    val fourthStudentGrade = 3f
+    val fourthStudentGrade = 5f
 
 
     println((firstStudentGrade + secondStudentGrade + thirdStudentGrade + fourthStudentGrade) / 4)
