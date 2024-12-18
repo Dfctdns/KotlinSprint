22678cf7a5acfe26b4fef7d4985d749920904743
dfctdns <denisbezrukov2006@gmail.com>
2024-12-18 19:22:14 +0300

    KS-3-1 created

---
+++ src/main/kotlin/lesson3/KS-3-1-greet.kt
@@ -0,0 +1,13 @@
+package org.example.lesson3
+
+fun main() {
+
+    val name = "Name"
+
+    var greet = "Доброе утро, $name!"
+    println(greet)
+
+    greet = "Добрый вечер, $name!"
+    println(greet)
+
+}
