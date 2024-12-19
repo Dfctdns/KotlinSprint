1daf98512da41f0fa30ee398975ef2e80cd29a33
dfctdns <denisbezrukov2006@gmail.com>
2024-12-19 13:27:14 +0300

    KS-2-2 created

---
+++ src/main/kotlin/lesson2/KS-2-2.kt
@@ -0,0 +1,15 @@
+package org.example.lesson2
+
+
+fun main() {
+
+    val employeeSalary = 50 * 30_000
+    val internSalary = 30 * 20_000
+    val overallSalary = employeeSalary + internSalary
+    val averageSalary = overallSalary / 50
+
+    println(employeeSalary)
+    println(overallSalary)
+    println(averageSalary)
+
+}
