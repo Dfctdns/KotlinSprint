8405b7ff0ab2e2b70ff8fc841d6e6a179aa2a62d
dfctdns <denisbezrukov2006@gmail.com>
2024-12-19 21:04:12 +0300

    KS-2-3 fixed

--- src/main/kotlin/lesson2/KS-2-3-train.kt
+++ src/main/kotlin/lesson2/KS-2-3-train.kt
@@ -4,7 +4,8 @@ fun main() {
 
     val departureHour = 9
     val departureMinute = 39
-    val arrivalTime = departureHour * 60 + departureMinute + 457
+    val timeOfRoad = 457
+    val arrivalTime = departureHour * 60 + departureMinute + timeOfRoad
     val arrivalHour = arrivalTime / 60
     val arrivalMinute = arrivalTime % 60
 
