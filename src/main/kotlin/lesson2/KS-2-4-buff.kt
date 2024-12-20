e553fa07f38d9c7fd194696c1a9021b5abc52ff5
dfctdns <denisbezrukov2006@gmail.com>
2024-12-20 23:53:16 +0300

    KS-2-4 fixed

--- src/main/kotlin/lesson2/KS-2-4-buff.kt
+++ src/main/kotlin/lesson2/KS-2-4-buff.kt
@@ -1,13 +1,14 @@
 package org.example.lesson2
 
+const val PERCENTS = 100
 fun main() {
 
     val crystalOre = 7
     val ironOre = 11
     val buffPercent = 20
 
-    val buffedCrystalOre = (crystalOre * buffPercent / 100).toInt()
-    val buffedIronOre = (ironOre * buffPercent / 100).toInt()
+    val buffedCrystalOre = (crystalOre * buffPercent / PERCENTS).toInt()
+    val buffedIronOre = (ironOre * buffPercent / PERCENTS).toInt()
 
     println("$buffedCrystalOre, $buffedIronOre")
 
