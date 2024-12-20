b51706560161abdaf919a4a708ce416438ef26d3
dfctdns <denisbezrukov2006@gmail.com>
2024-12-20 23:50:58 +0300

    KS-2-3 fixed

--- src/main/kotlin/lesson2/KS-2-3-train.kt
+++ src/main/kotlin/lesson2/KS-2-3-train.kt
@@ -1,13 +1,14 @@
 package org.example.lesson2
 
+const val MINUTES = 60
 fun main() {
 
     val departureHour = 9
     val departureMinute = 39
     val timeOfRoad = 457
-    val arrivalTime = departureHour * 60 + departureMinute + timeOfRoad
-    val arrivalHour = arrivalTime / 60
-    val arrivalMinute = arrivalTime % 60
+    val arrivalTime = departureHour * MINUTES + departureMinute + timeOfRoad
+    val arrivalHour = arrivalTime / MINUTES
+    val arrivalMinute = arrivalTime % MINUTES
 
     println("$arrivalHour:$arrivalMinute")
 
