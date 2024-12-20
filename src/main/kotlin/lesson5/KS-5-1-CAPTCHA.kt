5fbba2a7afe9442afa19d9e1ef538e861596c188
dfctdns <denisbezrukov2006@gmail.com>
2024-12-20 21:12:05 +0300

    KS-5-1 created

---
+++ src/main/kotlin/lesson5/KS-5-1-CAPTCHA.kt
@@ -0,0 +1,11 @@
+package org.example.lesson5
+
+fun main() {
+
+    print("Решите пример: 2 + 2 = ")
+    var answer = readLine()?.toInt()
+
+    if(answer == 4) println("Добро пожаловать!")
+    else println("Доступ запрещен")
+
+}
