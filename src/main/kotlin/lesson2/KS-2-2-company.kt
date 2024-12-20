3dad297c94a06ee68069a7a2aff4f16093f48903
dfctdns <denisbezrukov2006@gmail.com>
2024-12-20 23:46:59 +0300

    KS-2-2 fixed

--- src/main/kotlin/lesson2/KS-2-2-company.kt
+++ src/main/kotlin/lesson2/KS-2-2-company.kt
@@ -3,13 +3,12 @@ package org.example.lesson2
 
 fun main() {
 
-
     val numberOfEmployee = 50
     val numberOfIntern = 30
     val employeeSalary = 30_000
     val internSalary = 20_000
-    val employeeSalaryAll = numberOfEmployee * 30_000
-    val internSalaryAll = numberOfIntern * 20_000
+    val employeeSalaryAll = numberOfEmployee * employeeSalary
+    val internSalaryAll = numberOfIntern * internSalary
     val overallSalary = employeeSalaryAll + internSalaryAll
     val averageSalary = overallSalary / (numberOfIntern + numberOfEmployee)
 
