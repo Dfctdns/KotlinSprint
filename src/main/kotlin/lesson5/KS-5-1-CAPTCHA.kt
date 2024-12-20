020347044cafe8abc07c6ec6ea3b84251bb3ebbc
dfctdns <denisbezrukov2006@gmail.com>
2024-12-20 21:20:12 +0300

    KS-5-1 fixed #1

--- src/main/kotlin/lesson5/KS-5-1-CAPTCHA.kt
+++ src/main/kotlin/lesson5/KS-5-1-CAPTCHA.kt
@@ -3,7 +3,7 @@ package org.example.lesson5
 fun main() {
 
     print("Решите пример: 2 + 2 = ")
-    var answer = readLine()?.toInt()
+    var answer = readln().toInt()
 
     if (answer == 4) println("Добро пожаловать!")
     else println("Доступ запрещен")
