d7882bbdfd26e470aec832914602482107b2ca38
dfctdns <denisbezrukov2006@gmail.com>
2024-12-23 20:27:43 +0300

    KS-4-4 fixed #2

--- src/main/kotlin/lesson4/KS-4-4-gym.kt
+++ src/main/kotlin/lesson4/KS-4-4-gym.kt
@@ -1,17 +1,12 @@
 package org.example.lesson4
 
-const val ARM_DAY = true
-const val LEG_DAY = false
-const val BACK_DAY = false
-const val ABS_DAY = true
-
 fun main() {
     var dayOfTraining = 5
-    val isEven = dayOfTraining % 2 == 1
+    val isEven = dayOfTraining % 2 == 0
     println("""
-        Упражнения для рук: ${ARM_DAY == isEven}
-        Упражнения для ног: ${LEG_DAY == isEven}
-        Упражнения для спины: ${BACK_DAY == isEven}
-        Упражнения для пресса: ${ABS_DAY == isEven}
+        Упражнения для рук: ${!isEven}
+        Упражнения для ног: ${isEven}
+        Упражнения для спины: ${isEven}
+        Упражнения для пресса: ${!isEven}
     """.trimIndent())
 }
