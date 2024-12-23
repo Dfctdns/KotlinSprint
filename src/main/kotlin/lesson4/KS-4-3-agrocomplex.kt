c655d60bd12c140aca7e6ac705d9ed21d0a5b62f
dfctdns <denisbezrukov2006@gmail.com>
2024-12-23 17:19:08 +0300

    KS-4-3 fixed

--- src/main/kotlin/lesson4/KS-4-3-agrocomplex.kt
+++ src/main/kotlin/lesson4/KS-4-3-agrocomplex.kt
@@ -1,6 +1,6 @@
 package org.example.lesson4
 
-const val SUNNY_WEATHER = true
+const val IS_SUNNY_WEATHER = true
 const val TENT_IS_OPENED = true
 const val AIR_HUMIDITY = 20
 const val SEASON = "winter"
@@ -11,7 +11,9 @@ fun main() {
     val tentNowOpened = true
     val airHumidity = 20
     val seasonNow = "winter"
+    val circumstancesCheck = todayWeatherIsSunny == IS_SUNNY_WEATHER && tentNowOpened == TENT_IS_OPENED
+            && airHumidity == AIR_HUMIDITY && seasonNow != SEASON
 
-    println("Благоприятные ли условия сейчас для роста бобовых? ${todayWeatherIsSunny == SUNNY_WEATHER && tentNowOpened == TENT_IS_OPENED && airHumidity == AIR_HUMIDITY && seasonNow != SEASON}")
+    println("Благоприятные ли условия сейчас для роста бобовых? ${circumstancesCheck}")
 
 }
