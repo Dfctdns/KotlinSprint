01295747db38b962ca7f17a983dbc25549bf852d
dfctdns <denisbezrukov2006@gmail.com>
2024-12-24 19:03:12 +0300

    KS-5-1 fixed #1

--- src/main/kotlin/lesson5/KS-5-1-CAPTCHA.kt
+++ src/main/kotlin/lesson5/KS-5-1-CAPTCHA.kt
@@ -1,11 +1,13 @@
 package org.example.lesson5
 
+const val PROBLEM = "2 + 2"
+const val CORRECT_ANSWER = 4
 fun main() {
 
-    print("Решите пример: 2 + 2 = ")
+    print("Решите пример: $PROBLEM = ")
     var answer = readln().toInt()
 
-    if (answer == 4) println("Добро пожаловать!")
+    if (answer == CORRECT_ANSWER) println("Добро пожаловать!")
     else println("Доступ запрещен")
 
 }
